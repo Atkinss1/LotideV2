3bdb1245ae8dadd23993d71e7176042dbf39bcbb
Atkinss <jordanatkins@live.com>
2024-05-07 19:14:49 -0600

    add: mocha & chai test suite

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+const tail = require('../tail');
+
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    expect(tail(["Hello", "Lighthouse", "Labs"])).to.eql(["Lighthouse", "Labs"]);
+  });
+
+  it('returns null if parameter is not an array', () => {
+    expect(tail('hello, Lighthouse, Labs')).to.equal(null);
+  });
+});
